f0bf71aba0e9c94101676d75e03f22b7e8336fd3
Floyd Jones <floyd-jones1@hotmail.com>
2021-07-17 19:29:42 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# grpcwebtext-parse
+
+A simple script that parsed grpc-web-text and converts it to a more human friendly form. Currently can only convert grpc-web-text into human not from human to grpc-web-text. (WIP: needs to figure out the other way)
+
+## What you need to run this?
+
+```
+node
+protoc
+```
+
+Please install protoc either through chocolately(Windows) or brew(Mac).
+
+## How to use?
+
+```
+yarn install
+yarn start [insert grpc text]
+```
---
+++ index.ts
@@ -0,0 +1,54 @@
+import { exec } from "child_process";
+import { promisify } from "util";
+import minimist from "minimist";
+import chalk from "chalk";
+
+var argv = minimist(process.argv.slice(2));
+const asyncExec = promisify(exec);
+const log = console.log;
+
+let grpcWebTexts: Array<string> = [
+  "AAAAADEKB2pvZmxveWQSJmh0dHBzOi8vcGljc3VtLnBob3Rvcy9pZC8xMzMvMjc0Mi8xODI4",
+  "AAAAAAMInRQ=gAAAACBncnBjLXN0YXR1czowDQpncnBjLW1lc3NhZ2U6T0sNCg==",
+];
+
+function getBinaryHex() {
+  const buffers: Array<string> = [];
+  for (const text of grpcWebTexts) {
+    const buffer = Buffer.from(text, "base64");
+    const bufString = buffer.toString("hex");
+    buffers.push(bufString);
+  }
+  return buffers;
+}
+
+async function main() {
+  const { _: text } = argv;
+  if (text.length === 0) {
+    console.error("no input grpc text, defaulting to demo values");
+  } else {
+    grpcWebTexts = text;
+  }
+
+  // bytes are in base 16
+  const buffers = getBinaryHex();
+  for (const i in buffers) {
+    const b = buffers[i];
+    const webText = grpcWebTexts[i];
+    if (b.substr(0, 2) !== "00") return log(chalk.bgRed("Cannot Parse"));
+
+    const frameLen = b.substr(2, 4 * 2);
+
+    // convert to base 10
+    const len = parseInt(frameLen, 16);
+    const data = b.substr(10, len * 2);
+
+    const { stdout } = await asyncExec(
+      `echo ${data} | xxd -r -p | protoc --decode_raw`
+    );
+    log(`\n${chalk.bgBlue("Web gRPC text:")} ${webText}`);
+    console.log(stdout);
+  }
+}
+
+main();
